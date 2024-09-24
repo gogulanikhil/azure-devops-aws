57685366dfafb923533b023d075d0b1319514af3
Nikhil Gogula <nikhil.g@nexwave.in>
2024-09-24 07:21:30 +0000

    s3 removed main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-terraform {
-  required_version = ">= 1.3.14, <= 1.9.5" # Updated version constraint
-  required_providers {
-    aws = {
-      version = ">= 3.0.0, <= 5.0.0" # Updated provider version constraint
-      source = "hashicorp/aws"
-    }
-  }
-}
+// terraform {
+//   required_version = ">= 1.3.14, <= 1.9.5" # Updated version constraint
+//   required_providers {
+//     aws = {
+//       version = ">= 3.0.0, <= 5.0.0" # Updated provider version constraint
+//       source = "hashicorp/aws"
+//     }
+//   }
+// }
 
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
